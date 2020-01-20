082db35f759db0d4294ad21c568f2a122d7d828f
Christian Berndt <christian.berndt@liferay.com>
2020-01-20 22:29:20 +0100

    Add local service test.

---
+++ liferay-todo-list-test/src/testIntegration/java/com/chberndt/liferay/todo/list/service/persistence/test/TaskLocalServiceTest.java
@@ -0,0 +1,87 @@
+package com.chberndt.liferay.todo.list.service.persistence.test;
+
+import com.chberndt.liferay.todo.list.model.Task;
+import com.chberndt.liferay.todo.list.service.TaskLocalService;
+import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.portal.kernel.model.Group;
+import com.liferay.portal.kernel.model.User;
+import com.liferay.portal.kernel.service.ServiceContext;
+import com.liferay.portal.kernel.test.rule.AggregateTestRule;
+import com.liferay.portal.kernel.test.rule.DeleteAfterTestRun;
+import com.liferay.portal.kernel.test.util.GroupTestUtil;
+import com.liferay.portal.kernel.test.util.RandomTestUtil;
+import com.liferay.portal.kernel.test.util.ServiceContextTestUtil;
+import com.liferay.portal.kernel.test.util.TestPropsValues;
+import com.liferay.portal.kernel.util.CalendarFactoryUtil;
+import com.liferay.portal.service.test.ServiceTestUtil;
+import com.liferay.portal.test.rule.Inject;
+import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
+
+import java.util.Calendar;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(Arquillian.class)
+public class TaskLocalServiceTest {
+	
+	@ClassRule
+	@Rule
+	public static final AggregateTestRule aggregateTestRule =
+		new LiferayIntegrationTestRule();
+	
+	@Before
+	public void setUp() throws Exception {
+		
+		_group = GroupTestUtil.addGroup();
+		_user = TestPropsValues.getUser();
+
+		ServiceTestUtil.setUser(TestPropsValues.getUser());
+
+	}
+	
+	@Test
+	public void testAddTask() throws Exception {
+		
+		int initialCount = _taskLocalService.getTasksCount();
+
+		addTask(false);
+
+		int actualCount = _taskLocalService.getTasksCount();
+
+		Assert.assertEquals(initialCount + 1, actualCount);
+	}
+	
+	
+	protected Task addTask(boolean completed) throws Exception {
+		
+		return addTask(_user.getUserId(), completed);
+		
+	}
+
+	protected Task addTask(long userId, boolean completed) throws Exception {
+
+		ServiceContext serviceContext = ServiceContextTestUtil.getServiceContext(_group.getGroupId(), userId);
+
+		Calendar dueDate = CalendarFactoryUtil.getCalendar(2020, 1, 1);
+
+		Task task = _taskLocalService.addTask(userId, RandomTestUtil.randomString(), RandomTestUtil.randomString(),
+				completed, dueDate.getTime(), serviceContext);
+
+		return task;
+	}
+	
+	@Inject
+	private TaskLocalService _taskLocalService;
+	
+	@DeleteAfterTestRun
+	private Group _group;
+
+	private User _user;
+
+
+}
