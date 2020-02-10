fb93c669679f85aeef89732abbe00253d32cc6aa
Christian Berndt <christian.berndt@liferay.com>
2020-02-10 15:05:55 +0100

    Create todo-list-web module.

---
+++ liferay-todo-list-web/src/main/java/com/chberndt/liferay/todo/list/portlet/constants/ToDoListPortletKeys.java
@@ -0,0 +1,11 @@
+package com.chberndt.liferay.todo.list.portlet.constants;
+
+/**
+ * @author berndt
+ */
+public class ToDoListPortletKeys {
+
+	public static final String TODOLIST =
+		"com_chberndt_liferay_todo_list_portlet_ToDoListPortlet";
+
+}
---
+++ liferay-todo-list-web/src/main/java/com/chberndt/liferay/todo/list/portlet/portlet/ToDoListPortlet.java
@@ -0,0 +1,30 @@
+package com.chberndt.liferay.todo.list.portlet.portlet;
+
+import com.chberndt.liferay.todo.list.portlet.constants.ToDoListPortletKeys;
+
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
+
+import javax.portlet.Portlet;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author berndt
+ */
+@Component(
+	immediate = true,
+	property = {
+		"com.liferay.portlet.display-category=category.sample",
+		"com.liferay.portlet.header-portlet-css=/css/main.css",
+		"com.liferay.portlet.instanceable=true",
+		"javax.portlet.display-name=ToDoList",
+		"javax.portlet.init-param.template-path=/",
+		"javax.portlet.init-param.view-template=/view.jsp",
+		"javax.portlet.name=" + ToDoListPortletKeys.TODOLIST,
+		"javax.portlet.resource-bundle=content.Language",
+		"javax.portlet.security-role-ref=power-user,user"
+	},
+	service = Portlet.class
+)
+public class ToDoListPortlet extends MVCPortlet {
+}
