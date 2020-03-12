0de31ae57a3c5e68c1aee31b09c7451b27214dcb
Christian Berndt <christian.berndt@liferay.com>
2020-03-12 14:38:19 +0100

    Add ToDo List to product menu.

---
+++ liferay-todo-list-web/src/main/java/com/chberndt/liferay/todo/list/internal/application/list/ToDoListPanelApp.java
@@ -0,0 +1,29 @@
+package com.chberndt.liferay.todo.list.internal.application.list;
+
+import com.chberndt.liferay.todo.list.web.constants.ToDoListPortletKeys;
+
+import com.liferay.application.list.BasePanelApp;
+import com.liferay.application.list.PanelApp;
+import com.liferay.application.list.constants.PanelCategoryKeys;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Christian Berndt
+ */
+@Component(
+	immediate = true,
+	property = {
+		"panel.app.order:Integer=200",
+		"panel.category.key=" + PanelCategoryKeys.SITE_ADMINISTRATION_CONTENT
+	},
+	service = PanelApp.class
+)
+public class ToDoListPanelApp extends BasePanelApp {
+
+	@Override
+	public String getPortletId() {
+		return ToDoListPortletKeys.TODO_LIST;
+	}
+
+}
