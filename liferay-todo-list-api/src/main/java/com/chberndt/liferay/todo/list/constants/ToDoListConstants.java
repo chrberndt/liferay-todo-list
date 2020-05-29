746662eddfe26b477845d53921cc3f57171dc14b
Christian Berndt <christian.berndt@liferay.com>
2020-05-29 14:02:09 +0200

    Add constants class.

---
+++ liferay-todo-list-api/src/main/java/com/chberndt/liferay/todo/list/constants/ToDoListConstants.java
@@ -0,0 +1,10 @@
+package com.chberndt.liferay.todo.list.constants;
+
+/**
+ * @author Christian Berndt
+ */
+public class ToDoListConstants {
+
+	public static final String RESOURCE_NAME = "com.chberndt.liferay.todo.list";
+
+}
