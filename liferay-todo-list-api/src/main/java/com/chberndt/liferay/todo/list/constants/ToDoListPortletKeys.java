6699b767caab7bacf1f46f79b83fa0d1f0c72496
Christian Berndt <christian.berndt@liferay.com>
2020-06-04 21:59:30 +0200

    Move PortletKeys to api module.

---
+++ liferay-todo-list-api/src/main/java/com/chberndt/liferay/todo/list/constants/ToDoListPortletKeys.java
@@ -0,0 +1,11 @@
+package com.chberndt.liferay.todo.list.constants;
+
+/**
+ * @author Christian Berndt
+ */
+public class ToDoListPortletKeys {
+
+	public static final String TODO_LIST =
+		"com_chberndt_liferay_todo_list_internal_portlet_ToDoListPortlet";
+
+}
