0dd802c257512111649d47ffc64506662cefdff8
Christian Berndt <christian.berndt@liferay.com>
2020-06-05 16:53:07 +0200

    Add missing author annotation.

--- liferay-todo-list-service/src/main/java/com/chberndt/liferay/todo/list/internal/security/permission/resource/ToDoListPortletResourcePermissionRegistrar.java
+++ liferay-todo-list-service/src/main/java/com/chberndt/liferay/todo/list/internal/security/permission/resource/ToDoListPortletResourcePermissionRegistrar.java
@@ -20,7 +20,7 @@ import org.osgi.service.component.annotations.Deactivate;
 import org.osgi.service.component.annotations.Reference;
 
 /**
- * Christian Berndt
+ * @author Christian Berndt
  */
 @Component(immediate = true, service = {})
 public class ToDoListPortletResourcePermissionRegistrar {
