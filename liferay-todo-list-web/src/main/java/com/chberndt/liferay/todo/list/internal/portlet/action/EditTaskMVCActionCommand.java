765d7bcdcfde593e6d15022666b63269b1fbca8c
Christian Berndt <christian.berndt@liferay.com>
2021-03-13 12:47:27 +0100

    Improve exception handling.

--- liferay-todo-list-web/src/main/java/com/chberndt/liferay/todo/list/internal/portlet/action/EditTaskMVCActionCommand.java
+++ liferay-todo-list-web/src/main/java/com/chberndt/liferay/todo/list/internal/portlet/action/EditTaskMVCActionCommand.java
@@ -3,6 +3,7 @@ package com.chberndt.liferay.todo.list.internal.portlet.action;
 import com.chberndt.liferay.todo.list.constants.ToDoListPortletKeys;
 import com.chberndt.liferay.todo.list.exception.NoSuchTaskException;
 import com.chberndt.liferay.todo.list.exception.TaskDueDateException;
+import com.chberndt.liferay.todo.list.exception.TaskTitleException;
 import com.chberndt.liferay.todo.list.model.Task;
 import com.chberndt.liferay.todo.list.service.TaskService;
 
@@ -23,6 +24,8 @@ import java.util.Date;
 
 import javax.portlet.ActionRequest;
 import javax.portlet.ActionResponse;
+import javax.portlet.PortletRequest;
+import javax.portlet.PortletURL;
 
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
@@ -39,7 +42,7 @@ import org.osgi.service.component.annotations.Reference;
 )
 public class EditTaskMVCActionCommand extends BaseMVCActionCommand {
 
-	protected void deleteTask(ActionRequest actionRequest) throws Exception {
+	protected void deleteTasks(ActionRequest actionRequest) throws Exception {
 		long taskId = ParamUtil.getLong(actionRequest, "taskId");
 
 		long[] deleteTaskIds = null;
@@ -65,21 +68,53 @@ public class EditTaskMVCActionCommand extends BaseMVCActionCommand {
 
 		try {
 			if (cmd.equals(Constants.ADD) || cmd.equals(Constants.UPDATE)) {
-				updateTask(actionRequest);
+				Task task = updateTask(actionRequest);
+
+				String redirect = getSaveAndContinueRedirect(
+					actionRequest, task);
+
+				sendRedirect(actionRequest, actionResponse, redirect);
 			}
 			else if (cmd.equals(Constants.DELETE)) {
-				deleteTask(actionRequest);
+				deleteTasks(actionRequest);
 			}
 		}
 
-		// TODO: catch TaskDueDateException | TaskTitleException
+		catch (TaskDueDateException | TaskTitleException exception) {
+			SessionErrors.add(actionRequest, exception.getClass());
 
-		catch (NoSuchTaskException | PrincipalException e) {
-			SessionErrors.add(actionRequest, e.getClass());
+			actionResponse.setRenderParameter(
+				"mvcRenderCommandName", "/edit_task");
+
+			hideDefaultSuccessMessage(actionRequest);
+		}
+		catch (NoSuchTaskException | PrincipalException exception) {
+			SessionErrors.add(actionRequest, exception.getClass());
 		}
 	}
 
-	protected void updateTask(ActionRequest actionRequest) throws Exception {
+	protected String getSaveAndContinueRedirect(
+		ActionRequest actionRequest, Task task) {
+
+		PortletURL portletURL = _portal.getControlPanelPortletURL(
+			actionRequest, ToDoListPortletKeys.TODO_LIST,
+			PortletRequest.RENDER_PHASE);
+
+		if (task != null) {
+			portletURL.setParameter("backURL", portletURL.toString());
+
+			portletURL.setParameter("mvcRenderCommandName", "/edit_task");
+			portletURL.setParameter("taskId", String.valueOf(task.getTaskId()));
+
+			String redirect = ParamUtil.getString(actionRequest, "redirect");
+
+			portletURL.setParameter("redirect", redirect);
+		}
+
+		return portletURL.toString();
+	}
+
+	protected Task updateTask(ActionRequest actionRequest) throws Exception {
 		ThemeDisplay themeDisplay = (ThemeDisplay)actionRequest.getAttribute(
 			WebKeys.THEME_DISPLAY);
 
@@ -111,13 +146,12 @@ public class EditTaskMVCActionCommand extends BaseMVCActionCommand {
 			Task.class.getName(), actionRequest);
 
 		if (taskId <= 0) {
-			_taskService.addTask(
+			return _taskService.addTask(
 				title, description, completed, dueDate, serviceContext);
 		}
-		else {
-			_taskService.updateTask(
-				taskId, title, description, completed, dueDate, serviceContext);
-		}
+
+		return _taskService.updateTask(
+			taskId, title, description, completed, dueDate, serviceContext);
 	}
 
 	@Reference
