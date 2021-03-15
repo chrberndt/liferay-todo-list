0fc9d904e86e10f1c8ee806935ae88f402c57aca
Christian Berndt <christian.berndt@liferay.com>
2021-03-15 22:29:57 +0100

    Add portlet finder.

---
+++ liferay-todo-list-web/src/main/java/com/chberndt/liferay/todo/list/internal/portlet/ToDoListPortletLayoutFinder.java
@@ -0,0 +1,29 @@
+package com.chberndt.liferay.todo.list.internal.portlet;
+
+import com.chberndt.liferay.todo.list.constants.ToDoListPortletKeys;
+
+import com.liferay.portal.kernel.portlet.BasePortletLayoutFinder;
+import com.liferay.portal.kernel.portlet.PortletLayoutFinder;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Christian Berndt
+ */
+@Component(
+	immediate = true,
+	property = "model.class.name=com.chberndt.liferay.todo.list.model.Task",
+	service = PortletLayoutFinder.class
+)
+public class ToDoListPortletLayoutFinder extends BasePortletLayoutFinder {
+
+	@Override
+	protected String[] getPortletIds() {
+		return _PORTLET_IDS;
+	}
+
+	private static final String[] _PORTLET_IDS = {
+		ToDoListPortletKeys.TODO_LIST
+	};
+
+}
